848c955d26a71110bcb0e26f5ac075e17478812d
Alexandra Farrow <afarrow304@triton.unm.edu>
2025-03-05 14:27:54 -0700

    Inital set up still working

---
+++ main.c
@@ -0,0 +1,30 @@
+#include<stdio>
+#include"pixel.h"
+
+//calculate the average of the r,g,b values in the domain individually.
+//That is, average all r colors and give a result,
+//all g colors and give a result,
+//and all b colors and give a result. 
+void average_colors(std::vector<Pixel> &pixel_list){
+	
+}
+//flips all pixels vertically.
+//For example, in the y-direction, pixel 600 should become pixel 0
+//and pixel 0 should become pixel 600. 
+//No modification in the x-direction. 
+void flip_vertically(std::vector<Pixel_list){
+
+}
+//Pixel make_pixel(std::string line){
+//Pixel pixel;
+//int pos;
+//pos=line.find(',');//find the coma
+//pixel.x=std::stoi(line.substr(0,pos))//extract X val
+//line=line.substr(pos+1)//new line
+//use(std::ifstream)for reading
+//use (std::ofstream)for writing
+int main(){
+	std::vector<Pixel> pixel_list;
+	return 0;
+
+}
